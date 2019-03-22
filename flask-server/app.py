ae8944793447d130095f130156847a26c500417e
vnck <ivanckj96@gmail.com>
2019-03-22 14:58:10 +0800

    fixes

--- flask-server/app.py
+++ flask-server/app.py
@@ -9,6 +9,7 @@ Created on Tue Mar 19 14:06:41 2019
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 bcrypt = Bcrypt(app)
