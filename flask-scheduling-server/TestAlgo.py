cc3ae2627eddcfae07fd312e1793c7421c334240
Gary Ong <garyong.ahs@gmail.com>
2019-04-09 21:05:32 +0800

    testing class

--- flask-scheduling-server/TestAlgo.py
+++ flask-scheduling-server/TestAlgo.py
@@ -21,5 +21,20 @@ class TestAlgo(unittest.TestCase):
         models.createTestDB("data2/")
         Scheduler.startAlgo()
 
+    @unittest.expectedFailure
+    def test_unsatisfiable(self):
+        """
+        This dataset cannot be satisfied because a lecturer has more lessons than time
+        """
+        models.createTestDB("data3/")
+        Scheduler.startAlgo()
+
+    @unittest.expectedFailure
+    def test_noInstructor(self):
+        """
+        This dataset fails because there is no instructor for a particular course
+        """
+        models.createTestDB("data4/")
+        Scheduler.startAlgo()
 if __name__ == "__main__":
     unittest.main()
