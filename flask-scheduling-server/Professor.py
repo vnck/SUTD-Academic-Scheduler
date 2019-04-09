5751a6e485ae33df80e6605a684db136dbd15ec4
vnck <ivanckj96@gmail.com>
2019-04-09 14:49:26 +0800

    conflict management

--- flask-scheduling-server/Professor.py
+++ flask-scheduling-server/Professor.py
@@ -1,45 +1,46 @@
 class Professor:
-    
-    def __init__(self,name):
+
+    def __init__(self, name):
         """course is list of course objects"""
         self.name = name
         self._course = []
         self.slots = []
         self.courseClasses = []
 
-        #for soft constraints
+        # for soft constraints
         self.day = None
         self.startTime = None
         self.endTime = None
-        self.penatly = 0
-    def addCourse(self,course):
+        self.penalty = 0
+
+    def addCourse(self, course):
         """adds Course Object to list"""
         self._course.append(course)
         course.professors.append(self)
 
-    def addSlot(self,slot):
+    def addSlot(self, slot):
         self.slots.append(slot)
-    
-    #constraint prof cannot be at 2 places at any point in time
+
+    # constraint prof cannot be at 2 places at any point in time
     def fitness(self):
-        penatly = 0
+        penalty = 0
         for s in range(len(self.slots)-1):
-            for i in range(s+1,len(self.slots)):
-                #if day and period is the same we add
+            for i in range(s+1, len(self.slots)):
+                # if day and period is the same we add
                 if self.slots[s].day == self.slots[i].day\
-                    and self.slots[s].period == self.slots[i].period:
-                    penatly +=1
-        
-        #check soft constraints
+                        and self.slots[s].period == self.slots[i].period:
+                    penalty += 1
+
+        # check soft constraints
         if self.startTime != None and self.endTime != None and self.day != None:
             for slot in self.slots:
                 if slot.day == self.day:
-                    if slot.period<=self.endTime and slot.period>= self.startTime:
-                        #penalise once
-                        penatly+=0.01
+                    if slot.period <= self.endTime and slot.period >= self.startTime:
+                        # penalise once
+                        penalty += 0.01
                         break
-        self.penatly = penatly
-        return penatly
+        self.penalty = penalty
+        return penalty
 
     def __repr__(self):
-        return"<Instructor:{},course:{}>".format(self.name,self._course)
+        return"<Instructor:{},course:{}>".format(self.name, self._course)
--- flask-scheduling-server/StudentGroup.py
+++ flask-scheduling-server/StudentGroup.py
@@ -1,5 +1,5 @@
 class StudentGroup:
-    def __init__(self,name):
+    def __init__(self, name):
         """
         attr:name,course
         note course is a list of course objects
@@ -7,33 +7,36 @@ class StudentGroup:
         self.name = name
         self._course = []
         self.slots = []
-        
+
         self.courseClasses = []
-    def addCourse(self,course):
+
+    def addCourse(self, course):
         """adds course object"""
         self._course.append(course)
-        #adds student groups to course object
+        # adds student groups to course object
         course.studentGroups.append(self)
+
     def getCourses(self):
         return self._course
 
     def fitness(self):
-        penatly = 0
-        #check time space constraints
+        penalty = 0
+        # check time space constraints
         for s in range(len(self.slots)-1):
-            for i in range(s+1,len(self.slots)):
-                #if day and period is the same we add
+            for i in range(s+1, len(self.slots)):
+                # if day and period is the same we add
                 if self.slots[s].day == self.slots[i].day\
-                    and self.slots[s].period == self.slots[i].period:
-                    penatly +=1
-        
-        #Check that no two of the same course in a single day
-        #cannot have the same subject twice in a single day 
+                        and self.slots[s].period == self.slots[i].period:
+                    penalty += 1
+
+        # Check that no two of the same course in a single day
+        # cannot have the same subject twice in a single day
         for i in range(len(self.courseClasses)-1):
-            for j in range(s+1,len(self.courseClasses)):
+            for j in range(s+1, len(self.courseClasses)):
                 if self.courseClasses[i].day == self.courseClasses[j].day\
-                    and self.courseClasses[i].course == self.courseClasses[j].course:
-                    penatly +=1
-        return penatly
+                        and self.courseClasses[i].course == self.courseClasses[j].course:
+                    penalty += 1
+        return penalty
+
     def __repr__(self):
-        return "<Student Group:{} Courses:{} Slots:{}>".format(self.name,self._course,len(self.slots))
+        return "<Student Group:{} Courses:{} Slots:{}>".format(self.name, self._course, len(self.slots))
